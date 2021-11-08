d79ae841da2aa19a95aaeb2af4a106d6f340d36b
xieyezi <1435398529@qq.com>
2021-11-08 17:46:43 +0800

    feat: 工作台雷达图

---
+++ src/common/echarts.ts
@@ -0,0 +1,77 @@
+import * as echarts from 'echarts/core'
+import {
+	// LineChart,
+	BarChart,
+	// PieChart,
+	// ScatterChart,
+	RadarChart
+	// MapChart,
+	// TreeChart,
+	// TreemapChart,
+	// GraphChart,
+	// GaugeChart,
+	// FunnelChart,
+	// ParallelChart,
+	// SankeyChart,
+	// BoxplotChart,
+	// CandlestickChart,
+	// EffectScatterChart,
+	// LinesChart,
+	// HeatmapChart,
+	// PictorialBarChart,
+	// ThemeRiverChart,
+	// SunburstChart,
+	// CustomChart,
+} from 'echarts/charts'
+// import components, all suffixed with Component
+import {
+	// GridSimpleComponent,
+	GridComponent,
+	// PolarComponent,
+	RadarComponent,
+	// GeoComponent,
+	// SingleAxisComponent,
+	// ParallelComponent,
+	// CalendarComponent,
+	// GraphicComponent,
+	// ToolboxComponent,
+	TooltipComponent,
+	// AxisPointerComponent,
+	// BrushComponent,
+	TitleComponent,
+	// TimelineComponent,
+	// MarkPointComponent,
+	// MarkLineComponent,
+	// MarkAreaComponent,
+	LegendComponent,
+	// LegendScrollComponent,
+	// LegendPlainComponent,
+	// DataZoomComponent,
+	// DataZoomInsideComponent,
+	// DataZoomSliderComponent,
+	// VisualMapComponent,
+	// VisualMapContinuousComponent,
+	// VisualMapPiecewiseComponent,
+	// AriaComponent,
+	// TransformComponent,
+	DatasetComponent
+} from 'echarts/components'
+// Import renderer, note that introducing the CanvasRenderer or SVGRenderer is a required step
+import {
+	CanvasRenderer
+	// SVGRenderer,
+} from 'echarts/renderers'
+
+// Register the required components
+echarts.use([
+	TitleComponent,
+	TooltipComponent,
+	LegendComponent,
+	RadarComponent,
+	GridComponent,
+	BarChart,
+	RadarChart,
+	CanvasRenderer
+])
+
+export default echarts
--- src/pages/dashboard/workbeach/index.tsx
+++ src/pages/dashboard/workbeach/index.tsx
@@ -1,5 +1,9 @@
-import { Avatar, Descriptions, Col, Row, CardGroup, Card, Typography, List } from '@douyinfe/semi-ui'
 import React from 'react'
+import { IconPlus } from '@douyinfe/semi-icons'
+import { Avatar, Descriptions, Col, Row, CardGroup, Card, Typography, List, Button } from '@douyinfe/semi-ui'
+import { chinaNumChar } from '@src/common'
+import echarts from '@src/common/echarts'
+import ReactEChartsCore from 'echarts-for-react/lib/core'
 import './index.scss'
 
 const { Meta } = Card
@@ -93,8 +97,52 @@ const motion = [
 		action: '发布了',
 		project: '留言',
 		color: 'amber'
+	},
+	{
+		user: '赵大大',
+		group: '工程效能',
+		action: '发布了',
+		project: '留言',
+		color: 'red'
+	},
+	{
+		user: '李冰冰',
+		group: '工程效能',
+		action: '发布了',
+		project: '留言',
+		color: 'light-green'
 	}
 ]
+
+const option = {
+	radar: {
+		shape: 'circle',
+		indicator: [
+			{ name: 'Sales', max: 6500 },
+			{ name: 'Administration', max: 16000 },
+			{ name: 'Information Technology', max: 30000 },
+			{ name: 'Customer Support', max: 38000 },
+			{ name: 'Development', max: 52000 },
+			{ name: 'Marketing', max: 25000 }
+		]
+	},
+	series: [
+		{
+			name: 'Budget vs spending',
+			type: 'radar',
+			data: [
+				{
+					value: [4200, 3000, 20000, 35000, 50000, 18000],
+					name: 'Allocated Budget'
+				},
+				{
+					value: [5000, 14000, 28000, 26000, 42000, 21000],
+					name: 'Actual Spending'
+				}
+			]
+		}
+	]
+}
 
 const Index: React.FC = () => {
 	return (
@@ -109,7 +157,7 @@ const Index: React.FC = () => {
 							size="large"
 						/>
 						<div className="workbeach-container-header-content-left-user">
-							<p className="workbeach-container-header-content-left-username">早安，吴彦祖，祝你开心每一天！</p>
+							<p className="workbeach-container-header-content-left-username">早安，胡歌，祝你开心每一天！</p>
 							<p className="workbeach-container-header-content-left-user-profile">
 								前端专家 | 字节跳动－某某某事业群－某某平台部
 							</p>
@@ -152,7 +200,7 @@ const Index: React.FC = () => {
 							</Card>
 						</div>
 						<div className="workbeach-container-content-left2">
-							<Card title="最近动态" bordered={false}>
+							<Card title="最近动态" headerExtraContent={<Text link>更多</Text>} bordered={false}>
 								<List
 									size="large"
 									dataSource={motion}
@@ -178,7 +226,29 @@ const Index: React.FC = () => {
 						</div>
 					</Col>
 					<Col span={8}>
-						<div style={{ backgroundColor: 'white' }}>col-8</div>
+						<div>
+							<Card title="快捷导航">
+								<Row>
+									{new Array(6).fill(null).map((_, index) => (
+										<Col span={6} key={index}>
+											<Button type="tertiary" theme="borderless">
+												操作{chinaNumChar[index + 1]}
+											</Button>
+										</Col>
+									))}
+									<Button icon={<IconPlus />}>添加</Button>
+								</Row>
+							</Card>
+							<Card title="销售指数" style={{ marginTop: '20px' }}>
+								<ReactEChartsCore
+									echarts={echarts}
+									option={option}
+									notMerge={true}
+									lazyUpdate={true}
+									style={{ height: '400px' }}
+								/>
+							</Card>
+						</div>
 					</Col>
 				</Row>
 			</div>
