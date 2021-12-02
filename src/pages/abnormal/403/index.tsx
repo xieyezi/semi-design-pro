2eaef22145695897d77c380422d95870f3760e72
yuqing521 <1581446178@qq.com>
2021-12-02 22:19:09 +0800

    feat: add abnormal pages

--- src/pages/abnormal/403/index.tsx
+++ src/pages/abnormal/403/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Empty } from '@douyinfe/semi-ui'
+import { IllustrationNoAccess, IllustrationNoAccessDark } from '@douyinfe/semi-illustrations'
 
 const Index: React.FC = () => {
-	return <div>空白页</div>
+	return (
+		<Empty
+			title={'403'}
+			image={<IllustrationNoAccess style={{ width: 400, height: 400 }} />}
+			darkModeImage={<IllustrationNoAccessDark style={{ width: 400, height: 400 }} />}
+			description="403 无权限访问"
+		></Empty>
+	)
 }
 
 export default Index
--- src/pages/abnormal/500/index.tsx
+++ src/pages/abnormal/500/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Empty } from '@douyinfe/semi-ui'
+import { IllustrationFailure, IllustrationFailureDark } from '@douyinfe/semi-illustrations'
 
 const Index: React.FC = () => {
-	return <div>空白页</div>
+	return (
+		<Empty
+			title={'500'}
+			image={<IllustrationFailure style={{ width: 400, height: 400 }} />}
+			darkModeImage={<IllustrationFailureDark style={{ width: 400, height: 400 }} />}
+			description="500 服务器异常"
+		></Empty>
+	)
 }
 
 export default Index
