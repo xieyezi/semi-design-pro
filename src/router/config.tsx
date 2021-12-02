5ad2a78fef885feff133426b3639a3e765f4d4a1
yuqing521 <1581446178@qq.com>
2021-12-02 22:51:34 +0800

    chore: fix typo of pravateRoute

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -1,7 +1,7 @@
 import React, { FC, Suspense } from 'react'
 import { Route } from 'react-router-dom'
 import { RouteProps } from 'react-router'
-import PrivateRoute from './pravateRoute'
+import PrivateRoute from './privateRoute'
 import SuspendFallbackLoading from '@src/components/fallback-loading'
 
 export interface WrapperRouteProps extends RouteProps {
